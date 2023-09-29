88a61f3e8dbea94344b931620cdd2bf43856a2a8
Flamarine <William27060827@hotmail.com>
2023-09-29 23:51:23 +0800

    Initial commit

---
+++ src/main/scala/turniplabs/examplemod/ExampleMod.scala
@@ -0,0 +1,18 @@
+package turniplabs.examplemod
+
+import net.fabricmc.api.ModInitializer
+import net.fabricmc.loader.api.FabricLoader
+import org.slf4j.{Logger, LoggerFactory}
+
+object ExampleMod extends ModInitializer {
+	val MODID: String = "examplemod"
+	val LOGGER: Logger = LoggerFactory.getLogger(MODID)
+
+	override def onInitialize(): Unit = {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+		LOGGER.info("Initialized.")
+	}
+}
