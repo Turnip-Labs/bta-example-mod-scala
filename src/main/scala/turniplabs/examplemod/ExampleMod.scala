121402d5465b6d557c80b732bb0d4a2166b74165
Flamarine <William27060827@hotmail.com>
2023-09-30 00:41:03 +0800

    welp

--- src/main/scala/turniplabs/examplemod/ExampleMod.scala
+++ src/main/scala/turniplabs/examplemod/ExampleMod.scala
@@ -5,8 +5,8 @@ import net.fabricmc.loader.api.FabricLoader
 import org.slf4j.{Logger, LoggerFactory}
 
 object ExampleMod extends ModInitializer {
-	val MODID: String = "examplemod"
-	val LOGGER: Logger = LoggerFactory.getLogger(MODID)
+	final val MODID: String = "examplemod"
+	final val LOGGER: Logger = LoggerFactory.getLogger(MODID)
 
 	override def onInitialize(): Unit = {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
