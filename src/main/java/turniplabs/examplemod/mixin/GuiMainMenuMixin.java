8af774cc60f2b8c1c3cc2115c22d8d5e5824cdbb
Flamarine <William27060827@hotmail.com>
2023-09-30 00:13:44 +0800

    blastix riotz

--- src/main/java/turniplabs/examplemod/mixin/GuiMainMenuMixin.java
+++ src/main/java/turniplabs/examplemod/mixin/GuiMainMenuMixin.java
@@ -5,12 +5,12 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import turniplabs.examplemod.ExampleMod;
+import turniplabs.examplemod.ExampleMod$;
 
 @Mixin(value = GuiMainMenu.class, remap = false)
 public class GuiMainMenuMixin {
     @Inject(method = "<clinit>", at = @At("HEAD"))
     private static void examplemod$onClinit(CallbackInfo ci) {
-        ExampleMod.LOGGER.info("This line is printed by the example Mixin!");
+        ExampleMod$.MODULE$.LOGGER().info("This line is printed by the example Mixin!");
     }
 }
